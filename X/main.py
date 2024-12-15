cfaa6453974c1f4286a5f2222c8b0be52c9440fa
Rishi Kumar <rishi.kumar.2837@gmail.com>
2024-12-14 23:47:25 -0500

    Update main.py with separate p1, p2

--- X/main.py
+++ X/main.py
@@ -12,15 +12,21 @@ def part2(x):
 if __name__ == "__main__":
     part1_test = """"""
 
-    part2_test = """"""
+    part2_test = part1_test
 
     test_file = read_str("input.txt")
 
-    print("Part 1 (test):", part1(part1_test))
-    print("Part 2 (test):", part2(part2_test))
+    p1_tst = part1(part1_test)
+    print("Part 1 (test):", p1_tst)
 
-    # assert part1(part1_test)
-    # assert part2(part2_test)
+    # assert part1(part1_test) ==
 
     print("Part 1:", part1(test_file))
+
+
+    p2_tst = part2(part2_test)
+    print("Part 2 (test):", part2(part2_test))
+
+    # assert part2(part2_test) ==
+
     print("Part 2:", part2(test_file))
